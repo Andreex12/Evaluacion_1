d6134eee4463bbbc49aa6239b4c0c6921ca0f20d
andrescampos12 <55531120+andrescampos12@users.noreply.github.com>
2019-09-26 09:47:39 +0200

    Add files via upload

---
+++ Saludador_2_Andres.py
@@ -0,0 +1,7 @@
+def Saludador_2():
+    print "Hola amigo" #Instruccion de salida
+    nombre=raw_input("Como te llamas?:") #Instruccion de entrada
+    print "Bienvenido a Python "+nombre
+
+Saludador_2()
+
