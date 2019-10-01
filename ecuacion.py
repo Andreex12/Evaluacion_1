922672b88a035ceb3789d50a01db88e680a96ade
andrescampos12 <55531120+andrescampos12@users.noreply.github.com>
2019-10-01 12:02:26 +0200

    Add files via upload

---
+++ ecuacion.py
@@ -0,0 +1,17 @@
+#En este programa le pedimos al usuario que teclee los coeficientes de un polinomio y hallamos el valor de las raices
+def ecuacion():
+    print "Introduzca los coeficientes del polinomio"
+    print "A*x^2+B*x+C=0"
+    a=input("A= ")
+    b=input("B= ")
+    c=input("C= ")
+    raiz1=(-b+(b*b-4*a*c)^(1/2))/(2*a)
+    raiz2=(-b-(b*b-4*a*c)^(1/2))/(2*a)
+    print "primera solucion"
+    print raiz1
+    print "segunda solucion"
+    print raiz2
+   
+    
+ecuacion()
+    
